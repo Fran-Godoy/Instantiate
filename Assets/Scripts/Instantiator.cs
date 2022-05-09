36307fe64b1b28ed40489a0c45fd04497ecd9ec0
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-05-09 10:43:48 -0300

    instantiate completion

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CloneObject()
+    {
+        int cont=0;
+        while (cont<3)
+        {
+           Instantiate(objectToClone);
+            Debug.Log("Clonaste un cubo");
+            cont++;
+        }
+
+            
+        
+    }
+}
